7a8fab84abe9b939ba9ce7a25c02d1686b025e8c
Skm-c <skam2006maks@gmail.com>
2024-11-10 16:17:50 +0300

    Create many_one_files

---
+++ one_many_files/one_file/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+
+void merge(std::vector<int>& A, int p, int q, int r){
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    std::vector<int> L(n1 + 2);
+    std::vector<int> R(n2 + 2);
+    for (int i = 1; i < n1 + 1; ++i){
+        L[i] = A[p + i - 1];
+    }
+    for (int j = 1; j < n2 + 1; ++j){
+        R[j] = A[q + j];
+    }
+    L[n1 + 1] = 1000000000;
+    R[n2 + 1] = 1000000000;
+    int i = 1;
+    int j = 1;
+    
+    for (int k = p; k < r + 1; ++k){
+        if (L[i] < R[j]){
+            A[k] = L[i];
+            ++i;
+        } else{
+            A[k] = R[j];
+            ++j;    
+        }
+    }
+}
+
+void merge_sort(std::vector<int>& A, int p, int r){
+    if (p < r){
+        int q = (p + r)/2;
+        merge_sort(A, p, q);
+        merge_sort(A, q + 1, r);
+        merge(A, p, q, r);
+        
+    }
+}
+int main(){
+    std::cout << "Enter length of your array: " << std::endl;
+    int n;
+    std::cin >> n;
+    std::cout << "Enter numbers of your array: " << std::endl;
+	std::vector<int> A;
+    for (size_t i = 0; i < n; ++i){
+        int x;
+        std::cin >> x;
+        A.push_back(x);
+    }
+	int p = 0;
+	int r = n-1;
+	merge_sort(A, p, r);
+	std::cout << "Your sorted array: " << std::endl;
+	const char format = ' ';
+	for (size_t i = 0; i < n; ++i){
+        std::cout << A[i] << format;
+    }
+	
+}
--- refactoring/main.cpp
+++ refactoring/main.cpp
@@ -4,29 +4,29 @@
 
 void func(double angle){
 	double func1;
-	if (cos((double)2*angle) != 1){
-		func1 = (1 + sin((double)2*angle)) / (1 - cos((double)2*angle));
-		std::cout << "F(angle1)= " << func1 << std::endl;
+	if (cos(2*angle) != 1){
+		func1 = (1 + sin(2*angle)) /
+		(1 - cos(2*angle));
+		std::cout << "Func1(angle1)= " << func1 << std::endl;
 	} else {
-		std::cout << "Неверный ввод для F(angle1) " << std::endl;
+		std::cout << "Неверный ввод для Func1(angle1) " << std::endl;
 	}
 	
 	double func2;
-	if (tan((double)angle) != 1){
-		func2 = (1 + tan((double)angle)*tan((double)angle)) / 
-		(1 - tan((double)angle)*tan((double)angle));
-		std::cout << "F(angle2)= " << func2 << std::endl;
+	if (tan(angle) != 1){
+		func2 = (1 + tan(angle)*tan(angle)) / 
+		(1 - tan(angle)*tan(angle));
+		std::cout << "Func2(angle2)= " << func2 << std::endl;
 	} else {
-		std::cout << "Неверный ввод для F(angle2) " << std::endl;
+		std::cout << "Неверный ввод для Func2(angle2) " << std::endl;
 	}
 
 }
 
-int main(int argc, char* argv[]){
+int main(){
 	double angle;
 	std::cout << "Введите угол: " << std::endl;
 	std::cin >> angle;
 	func(angle);
-	system ("pause");
 	return 0;
 }
